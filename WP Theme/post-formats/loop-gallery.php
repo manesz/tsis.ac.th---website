561c4893b54e128ae5c32815ce52f8412730d9c8
Wararit Satitnimankan <manesz13@gmail.com>
2015-02-27 09:42:31 +0700

    Upload gallery library
    - upload fancybox library

--- WP Theme/post-formats/loop-gallery.php
+++ WP Theme/post-formats/loop-gallery.php
@@ -21,33 +21,34 @@ if(!$wp_query->found_posts){
 query_posts( $args );
 } 
 ?>
-<div class="container">
-<div class="row" id="container-gall">
-<?php
-while(have_posts()):the_post();
-$imagethumb = get_the_post_thumbnail($post->ID,'medium');
-	$gallimage = $imagethumb?str_replace('','',$imagethumb):get_first_inserted_image();
-	$myimages = $post->post_title;
-	$mylink = get_permalink($post->ID);
+
+	<div id="container-gall">
+	<?php
+		while(have_posts()):the_post();
+		$imagethumb = get_the_post_thumbnail($post->ID,'medium');
+		$gallimage = $imagethumb?str_replace('','',$imagethumb):get_first_inserted_image();
+		$myimages = $post->post_title;
+		$mylink = get_permalink($post->ID);
 	?>
-    <div class="col-md-3 camps">
-                <a href="<?php echo $mylink;?>"><?php echo $gallimage?></a>
-                <ul class="product_title titlelast">
-                    <li class="s_head"><h3><?php echo str_replace(array('_','-'),' ',$post->post_title);?></h3></li>
-                </ul>
-                <div class="clearfix"> </div>
-            </div>
-    <?php
-endwhile;?>
-</div></div><div class="clearfix"> </div><div class="ngg-navigation"></div>
+		<div class="col-md-3" style="height: 300px;">
+			<div class="gallery-frame"><a href="<?php echo $mylink;?>"><?php echo $gallimage?></a></div>
+			<p><?php echo str_replace(array('_','-'),' ',$post->post_title);?></p>
+		</div>
+	<?php endwhile;?>
+	</div>
+	
+<div class="clearfix"> </div>
+<div class="ngg-navigation"></div>
 <style type="text/css">
 .col-md-3.camps{margin-bottom:10px;overflow:hidden}.col-md-3.camps ul.product_title li.s_head h3{font-size:18px}
+div.gallery-frame a img.gallery-thumb { width: 100%; overflow: hidden; }
+img.attachment-medium.wp-post-image { max-width: 100%; min-height: 200px; }
 </style>
-<script type="text/javascript" src="<?php echo 
-get_stylesheet_directory_uri().'/library/js/masonry.pkgd.min.js';?>"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/library/js/masonry.pkgd.min.js';?>"></script>
 <script type="text/javascript">
-var container = document.querySelector('#container-gall');
-var msnry = new Masonry( container, {
-  itemSelector: '.camps'
-});
+	var container = document.querySelector('#container-gall');
+	var msnry = new Masonry( container, {
+	  itemSelector: '.camps'
+	});
+	$("img").addClass("gallery-thumb")
 </script>
